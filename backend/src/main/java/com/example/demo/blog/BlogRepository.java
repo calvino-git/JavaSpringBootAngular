62492742711d26f1bcd2918c38c495d7e87dd5ce
calvino <bbbebe661@yahoo.fr>
2025-08-15 23:32:42 +0100

    Ok

--- backend/src/main/java/com/example/demo/blog/BlogRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.blog;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogRepository extends JpaRepository<Blog, Integer> {
-    // Additional query methods can be defined here if needed
-    
-}
